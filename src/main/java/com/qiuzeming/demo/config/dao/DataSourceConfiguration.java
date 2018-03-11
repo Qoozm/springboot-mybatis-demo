1ec96743735348109be121e392f466c5d48553d2
qiuzeming <qiuzeming@meituan.com>
2018-03-11 13:53:36 +0800

    init project

---
+++ src/main/java/com/qiuzeming/demo/config/dao/DataSourceConfiguration.java
@@ -0,0 +1,43 @@
+package com.qiuzeming.demo.config.dao;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.PropertyVetoException;
+
+/**
+ * Created by qiuzeming on 2018/3/11.
+ */
+@Configuration
+
+//配置mybatis mapper文件的扫描路径
+@MapperScan("com.qiuzeming.demo.dao")
+public class DataSourceConfiguration {
+
+    @Value("${jdbc.driver}")
+    private String jdbcDriver;
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${jdbc.password}")
+    private String jdbcPassword;
+
+    @Value("jdbc.username")
+    private String jdbcUsername;
+
+    @Bean(name = "dataSource")
+    public ComboPooledDataSource createDataSource() throws PropertyVetoException {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setDriverClass(jdbcDriver);
+        comboPooledDataSource.setJdbcUrl(jdbcUrl);
+        comboPooledDataSource.setUser(jdbcUsername);
+        comboPooledDataSource.setPassword(jdbcPassword);
+        // 关闭连接后不自动提交  事务
+        comboPooledDataSource.setAutoCommitOnClose(false);
+        return comboPooledDataSource;
+    }
+}
