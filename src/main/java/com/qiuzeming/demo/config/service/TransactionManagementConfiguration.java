7880df92bb31a0160115a3ca1d46ffda86d16247
qiuzeming <qiuzeming@meituan.com>
2018-03-11 16:24:38 +0800

    service

---
+++ src/main/java/com/qiuzeming/demo/config/service/TransactionManagementConfiguration.java
@@ -0,0 +1,28 @@
+package com.qiuzeming.demo.config.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by qiuzeming on 2018/3/11.
+ */
+@Configuration
+@EnableTransactionManagement
+public class TransactionManagementConfiguration implements TransactionManagementConfigurer {
+
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
